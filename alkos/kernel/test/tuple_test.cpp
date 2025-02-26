feab4914c792ad9e349ff224ebceaefa37f750d3
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-02-26 23:29:20 +0100

    Added extended tuple tests

--- alkos/kernel/test/tuple_test.cpp
+++ alkos/kernel/test/tuple_test.cpp
@@ -45,9 +45,48 @@ TEST_F(TupleTest, TupleMixedTypes)
     EXPECT_EQ(4_s, std::tuple_size_v<decltype(tuple1)>);
 }
 
-TEST_F(TupleTest, TupleGet) {}
+TEST_F(TupleTest, TupleGet)
+{
+    auto tuple = std::make_tuple(42, "hello", 3.14);
 
-TEST_F(TupleTest, TupleElement) {}
+    EXPECT_EQ(42, std::get<0>(tuple));
+    EXPECT_STREQ("hello", std::get<1>(tuple));
+    EXPECT_EQ(3.14, std::get<2>(tuple));
+
+    std::get<0>(tuple) = 100;
+    EXPECT_EQ(100, std::get<0>(tuple));
+
+    const auto constTuple = std::make_tuple(1, 2.5, "test");
+    EXPECT_EQ(1, std::get<0>(constTuple));
+    EXPECT_EQ(2.5, std::get<1>(constTuple));
+    EXPECT_STREQ("test", std::get<2>(constTuple));
+
+    EXPECT_EQ(100, std::get<int>(tuple));
+    EXPECT_STREQ("hello", std::get<const char*>(tuple));
+    EXPECT_EQ(3.14, std::get<double>(tuple));
+
+    EXPECT_EQ(10, std::get<0>(std::make_tuple(10, 20, 30)));
+}
+
+TEST_F(TupleTest, TupleElement)
+{
+    using TupleType = std::tuple<int, double, const char*>;
+
+    EXPECT_TRUE((std::is_same_v<int, std::tuple_element_t<0, TupleType>>));
+    EXPECT_TRUE((std::is_same_v<double, std::tuple_element_t<1, TupleType>>));
+    EXPECT_TRUE((std::is_same_v<const char*, std::tuple_element_t<2, TupleType>>));
+
+    using ConstTupleType = const std::tuple<int, double, const char*>;
+    EXPECT_TRUE((std::is_same_v<const int, std::tuple_element_t<0, ConstTupleType>>));
+    EXPECT_TRUE((std::is_same_v<const double, std::tuple_element_t<1, ConstTupleType>>));
+    EXPECT_TRUE((std::is_same_v<const char* const, std::tuple_element_t<2, ConstTupleType>>));
+
+    using VolatileTupleType = volatile std::tuple<int, double, const char*>;
+    EXPECT_TRUE((std::is_same_v<volatile int, std::tuple_element_t<0, VolatileTupleType>>));
+
+    using CVTupleType = const volatile std::tuple<int, double, const char*>;
+    EXPECT_TRUE((std::is_same_v<const volatile int, std::tuple_element_t<0, CVTupleType>>));
+}
 
 TEST_F(TupleTest, TupleBindings)
 {
@@ -85,5 +124,116 @@ TEST_F(TupleTest, TupleBindings)
     EXPECT_TRUE((std::is_same_v<int, decltype(h)>));
     EXPECT_TRUE((std::is_same_v<double, decltype(i)>));
     EXPECT_TRUE((std::is_same_v<float, decltype(j)>));
-    EXPECT_TRUE((std::is_same_v<const char* const, decltype(k)>));
+    EXPECT_TRUE((std::is_same_v<const char*, decltype(k)>));
+}
+
+TEST_F(TupleTest, TupleHelpers)
+{
+    auto t1 = std::make_tuple(42, "test", 3.14);
+    EXPECT_EQ(42, std::get<0>(t1));
+    EXPECT_STREQ("test", std::get<1>(t1));
+    EXPECT_EQ(3.14, std::get<2>(t1));
+
+    int arr[3] = {1, 2, 3};
+    auto t2    = std::make_tuple(arr, 5);
+    EXPECT_TRUE((std::is_same_v<int*, std::tuple_element_t<0, decltype(t2)>>));
+
+    // TODO:
+    // // Test make_tuple with references using std::ref and std::cref
+    // int x = 42;
+    // auto t3 = std::make_tuple(std::ref(x), std::cref(x));
+    // x = 100;
+    // EXPECT_EQ(100, std::get<0>(t3));
+    // EXPECT_EQ(100, std::get<1>(t3));
+
+    // Test type correctness with ref
+    // EXPECT_TRUE((std::is_same_v<std::reference_wrapper<int>, std::tuple_element_t<0,
+    // decltype(t3)>>)); EXPECT_TRUE((std::is_same_v<std::reference_wrapper<const int>,
+    // std::tuple_element_t<1, decltype(t3)>>));
+}
+
+// TODO:
+// TEST_F(TupleTest, TupleComparison)
+// {
+//     auto tuple1 = std::make_tuple(1, 2.0, "a");
+//     auto tuple2 = std::make_tuple(1, 2.0, "a");
+//     auto tuple3 = std::make_tuple(2, 2.0, "a");
+//     auto tuple4 = std::make_tuple(1, 3.0, "a");
+//     auto tuple5 = std::make_tuple(1, 2.0, "b");
+//
+//     EXPECT_TRUE(tuple1 == tuple2);
+//     EXPECT_FALSE(tuple1 == tuple3);
+//     EXPECT_FALSE(tuple1 == tuple4);
+//     EXPECT_FALSE(tuple1 == tuple5);
+//
+//     EXPECT_FALSE(tuple1 != tuple2);
+//     EXPECT_TRUE(tuple1 != tuple3);
+//     EXPECT_TRUE(tuple1 != tuple4);
+//     EXPECT_TRUE(tuple1 != tuple5);
+//
+//     EXPECT_TRUE(tuple1 < tuple3);
+//     EXPECT_TRUE(tuple1 < tuple4);
+//     EXPECT_TRUE(tuple1 < tuple5);
+//     EXPECT_FALSE(tuple3 < tuple1);
+//
+//     EXPECT_TRUE(tuple1 <= tuple2);
+//     EXPECT_TRUE(tuple1 <= tuple3);
+//     EXPECT_TRUE(tuple1 <= tuple5);
+//     EXPECT_FALSE(tuple3 <= tuple1);
+//
+//     EXPECT_FALSE(tuple1 > tuple2);
+//     EXPECT_FALSE(tuple1 > tuple3);
+//     EXPECT_TRUE(tuple3 > tuple1);
+//     EXPECT_TRUE(tuple5 > tuple1);
+//
+//     EXPECT_TRUE(tuple1 >= tuple2);
+//     EXPECT_FALSE(tuple1 >= tuple3);
+//     EXPECT_TRUE(tuple3 >= tuple1);
+// }
+
+// TEST_F(TupleTest, TupleCopyMove)
+// {
+//     auto original = std::make_tuple(42, 3.14, "test");
+//     auto copy = original;
+//
+//     EXPECT_EQ(std::get<0>(original), std::get<0>(copy));
+//     EXPECT_EQ(std::get<1>(original), std::get<1>(copy));
+//     EXPECT_STREQ(std::get<2>(original), std::get<2>(copy));
+//
+//     std::get<0>(copy) = 100;
+//
+//     EXPECT_EQ(42, std::get<0>(original));
+//     EXPECT_STREQ("test", std::get<2>(original));
+//
+//     auto movable = std::make_tuple(42, 3.14, "test");
+//     auto moved = std::move(movable);
+//
+//     EXPECT_EQ(42, std::get<0>(moved));
+//     EXPECT_EQ(3.14, std::get<1>(moved));
+//     EXPECT_STREQ("test", std::get<2>(moved));
+//
+//     auto assigned = std::tuple(moved);
+//
+//     EXPECT_EQ(42, std::get<0>(assigned));
+//     EXPECT_EQ(3.14, std::get<1>(assigned));
+//     EXPECT_STREQ("test", std::get<2>(assigned));
+//
+//     // TODO
+//     // auto target = std::make_tuple(0, 0.0, "");
+//     // target = std::make_tuple(100, 2.71, "moved");
+//     //
+//     // EXPECT_EQ(100, std::get<0>(target));
+//     // EXPECT_EQ(2.71, std::get<1>(target));
+//     // EXPECT_STREQ("moved", std::get<2>(target));
+// }
+
+TEST_F(TupleTest, TupleEmptyConstruction)
+{
+    std::tuple<> emptyTuple{};
+    EXPECT_EQ(0_s, std::tuple_size_v<decltype(emptyTuple)>);
+
+    std::tuple<int, double, const char*> defaultTuple{};
+    EXPECT_EQ(0, std::get<0>(defaultTuple));
+    EXPECT_EQ(0.0, std::get<1>(defaultTuple));
+    EXPECT_EQ(nullptr, std::get<2>(defaultTuple));
 }
