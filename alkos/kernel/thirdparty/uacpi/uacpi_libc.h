ae13e69a60d6142cfce1460bd1b681a5490da2b8
F1r3d3v <ogieniewski.dev@gmail.com>
2025-02-26 20:20:16 +0100

    add uacpi library

---
+++ alkos/kernel/thirdparty/uacpi/uacpi_libc.h
@@ -0,0 +1,17 @@
+#ifndef KERNEL_THIRDPARTY_UACPI_UACPI_LIBC_H_
+#define KERNEL_THIRDPARTY_UACPI_UACPI_LIBC_H_
+
+#include <stdio.h>
+#include <string.h>
+
+#define uacpi_memcpy    memcpy
+#define uacpi_memmove   memmove
+#define uacpi_memset    memset
+#define uacpi_memcmp    memcmp
+#define uacpi_strlen    strlen
+#define uacpi_strnlen   strnlen
+#define uacpi_strcmp    strcmp
+#define uacpi_snprintf  snprintf
+#define uacpi_vsnprintf vsnprintf
+
+#endif  // KERNEL_THIRDPARTY_UACPI_UACPI_LIBC_H_
