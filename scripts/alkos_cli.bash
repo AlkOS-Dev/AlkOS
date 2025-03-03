5f708735008cd3f6aa5916813a8353c564306945
Adam Ogieniewski <ogieniewski.dev@gmail.com>
2025-03-03 01:11:34 +0100

    Workflow Improvements (#109)
    This PR includes several changes to improve the automation of the build
    and test environment:
    
    ### Improvements to GitHub Actions and Workflows:
    *
    [`.github/actions/prepare_env/action.yaml`](diffhunk://#diff-d2a643aadfc6be30476e47be7a45cfb559795c175427f011ec24e37c91279fb9R1-R50):
    Created a new composite action to prepare the environment for the build,
    including installing packages, configuring the environment, caching the
    toolchain, and building the toolchain if not cached.
    *
    [`.github/workflows/automated_tests.yaml`](diffhunk://#diff-0aca314923858c03b7dd31233cdc92afe2fa647ff0ab19572de5aeba83b6d1b0L13-R38):
    Updated the workflow to use the new `prepare_env` action, build AlkOS,
    run tests, and upload artifacts with improved naming and compression.
    
    ### Enhancements to Dependency Installation:
    *
    [`scripts/alkos_cli.bash`](diffhunk://#diff-d6fe9a3e8f641a3f60a80deb12392cfa03d1c044e26fff8f2ad85d8fe5471b73R20-R25):
    Refactored the dependency installation process to support multiple
    distributions by introducing a dictionary of install scripts and a
    function to detect the supported distribution.
    
    ### Toolchain Version Management:
    *
    [`scripts/env/build_cross_compile.bash`](diffhunk://#diff-ffc85218e152db2f3b304662092ffd89c58caf496080efbdff5bec7a0391e7c3R40-R72):
    Introduced a function to load toolchain versions from a file, ensuring
    that the correct versions are used during the build process.
    *
    [`scripts/env/toolchain_versions.txt`](diffhunk://#diff-20ff7723555c0c15f487fd6aec7eb7cf21d33b0e1d3d1672badf83949d5e3bb4R1-R3):
    Added a new file to specify the versions of binutils, gdb, and gcc to be
    used for cross-compilation.
    
    ### Cleanup and Removal of Redundant Scripts:
    *
    [`scripts/cicd/build_and_run_tests.bash`](diffhunk://#diff-3957d4d55ae6838785a506ad14a969ebbb41519142a59e41f17798cdf8914483L1-L21):
    Removed the script as its functionality has been integrated into the new
    composite GitHub Action and updated workflow.
    
    ### Bug Fixes:
    * Fixed the progress bar display that was polluting the screen during
    toolchain installing.

--- scripts/alkos_cli.bash
+++ scripts/alkos_cli.bash
@@ -8,7 +8,6 @@ readonly ALK_OS_CLI_SCRIPT_PATH="${ALK_OS_CLI_SCRIPT_DIR}/$(basename "$0")"
 # Script paths
 readonly ALK_OS_CLI_INSTALL_TOOLCHAIN_PATH="${ALK_OS_CLI_SCRIPT_DIR}/actions/install_toolchain.bash"
 readonly ALK_OS_CLI_BUILD_SCRIPT_PATH="${ALK_OS_CLI_SCRIPT_DIR}/actions/build_alkos.bash"
-readonly ALK_OS_CLI_INSTALL_DEPS_SCRIPT_PATH="${ALK_OS_CLI_SCRIPT_DIR}/env/install_deps_arch.bash"
 readonly ALK_OS_CLI_QEMU_RUN_SCRIPT_PATH="${ALK_OS_CLI_SCRIPT_DIR}/actions/run_alkos.bash"
 readonly ALK_OS_CLI_CONFIGURE_SCRIPT_PATH="${ALK_OS_CLI_SCRIPT_DIR}/configure.bash"
 readonly ALK_OS_CLI_CONF_PATH="${ALK_OS_CLI_SCRIPT_DIR}/conf.bash"
@@ -18,8 +17,14 @@ readonly ALK_OS_CLI_SETUP_HOOKS_SCRIPT_PATH="${ALK_OS_CLI_SCRIPT_DIR}/git-hooks/
 source "${ALK_OS_CLI_SCRIPT_DIR}/utils/pretty_print.bash"
 source "${ALK_OS_CLI_SCRIPT_DIR}/utils/helpers.bash"
 
+# Install dependencies scripts dictionary
+declare -A ALK_OS_CLI_INSTALL_DEPS_SCRIPT_PATH_DICT=(
+    [arch]="env/install_deps_arch.bash"
+    [ubuntu]="env/install_deps_ubuntu.bash"
+)
+
 # Default configuration
-declare -A CONFIG=(
+declare -A ALK_OS_CLI_CONFIG=(
     [run]=false
     [install_toolchain]=false
     [install_deps]=false
@@ -70,20 +75,20 @@ parse_args() {
                 exit 0
                 ;;
             -r|--run)
-                CONFIG[run]=true
+                ALK_OS_CLI_CONFIG[run]=true
                 shift
                 ;;
             -i|--install)
                 case $2 in
                     toolchain)
-                        CONFIG[install_toolchain]=true
+                        ALK_OS_CLI_CONFIG[install_toolchain]=true
                         ;;
                     deps)
-                        CONFIG[install_deps]=true
+                        ALK_OS_CLI_CONFIG[install_deps]=true
                         ;;
                     all)
-                        CONFIG[install_toolchain]=true
-                        CONFIG[install_deps]=true
+                        ALK_OS_CLI_CONFIG[install_toolchain]=true
+                        ALK_OS_CLI_CONFIG[install_deps]=true
                         ;;
                     *)
                         dump_error "Invalid install component: $2"
@@ -93,15 +98,15 @@ parse_args() {
                 shift 2
                 ;;
             -v|--verbose)
-                CONFIG[verbose]="-v"
+                ALK_OS_CLI_CONFIG[verbose]="-v"
                 shift
                 ;;
             -c|--configure)
-                CONFIG[configure]=true
+                ALK_OS_CLI_CONFIG[configure]=true
                 shift
                 ;;
             -g|--git-hooks)
-                CONFIG[setup_hooks]=true
+                ALK_OS_CLI_CONFIG[setup_hooks]=true
                 shift
                 ;;
             *)
@@ -113,35 +118,37 @@ parse_args() {
 }
 
 validate_args() {
-    if [[ ${CONFIG[run]} == false ]] &&
-       [[ ${CONFIG[install_toolchain]} == false ]] &&
-       [[ ${CONFIG[install_deps]} == false ]] &&
-       [[ ${CONFIG[configure]} == false ]] &&
-       [[ ${CONFIG[setup_hooks]} == false ]]; then
+    if [[ ${ALK_OS_CLI_CONFIG[run]} == false ]] &&
+       [[ ${ALK_OS_CLI_CONFIG[install_toolchain]} == false ]] &&
+       [[ ${ALK_OS_CLI_CONFIG[install_deps]} == false ]] &&
+       [[ ${ALK_OS_CLI_CONFIG[configure]} == false ]] &&
+       [[ ${ALK_OS_CLI_CONFIG[setup_hooks]} == false ]]; then
         dump_error "No action specified. Use --run, --install, --configure or --git-hooks."
         exit 1
     fi
 }
 
 run_default_configuration() {
-    if [[ ${CONFIG[configure]} == true ]]; then
+    if [[ ${ALK_OS_CLI_CONFIG[configure]} == true ]]; then
         pretty_info "Running default configuration"
         base_runner "Failed to run default configuration" true \
-            "${ALK_OS_CLI_CONFIGURE_SCRIPT_PATH}" x86_64 debug_qemu ${CONFIG[verbose]}
+            "${ALK_OS_CLI_CONFIGURE_SCRIPT_PATH}" x86_64 debug_qemu ${ALK_OS_CLI_CONFIG[verbose]}
     fi
 }
 
 install_dependencies() {
-    if [[ ${CONFIG[install_deps]} == true ]]; then
-        pretty_info "Installing system dependencies"
-        check_runs_on_arch
-        if [[ $? -ne 0 ]]; then
-            dump_error "Dependencies installation only supported on Arch Linux"
-            exit 1
-        fi
+    if [[ ${ALK_OS_CLI_CONFIG[install_deps]} == true ]]; then
+      pretty_info "Installing system dependencies"
 
-        base_runner "Failed to install dependencies" true \
-            "${ALK_OS_CLI_INSTALL_DEPS_SCRIPT_PATH}" --install ${CONFIG[verbose]}
+      local distro=$(get_supported_distro_name)
+      if [[ -z $distro ]]; then
+        dump_error "Dependencies installation is not supported on this distribution"
+        exit 1
+      fi
+
+      local install_script_path="${ALK_OS_CLI_SCRIPT_DIR}/${ALK_OS_CLI_INSTALL_DEPS_SCRIPT_PATH_DICT[$distro]}"
+      base_runner "Failed to install dependencies" true \
+        "${install_script_path}" --install ${ALK_OS_CLI_CONFIG[verbose]}
     fi
 }
 
@@ -153,21 +160,21 @@ validate_configuration_exists() {
 }
 
 install_toolchain() {
-    if [[ ${CONFIG[install_toolchain]} == true ]]; then
+    if [[ ${ALK_OS_CLI_CONFIG[install_toolchain]} == true ]]; then
         validate_configuration_exists
 
-        pretty_info "Installing cross-compile toolchain for ${CONFIG[arch]}"
+        pretty_info "Installing cross-compile toolchain for ${ALK_OS_CLI_CONFIG[arch]}"
         base_runner "Failed to install cross-compile toolchain" true \
-            "${ALK_OS_CLI_INSTALL_TOOLCHAIN_PATH}" ${CONFIG[verbose]}
+            "${ALK_OS_CLI_INSTALL_TOOLCHAIN_PATH}" ${ALK_OS_CLI_CONFIG[verbose]}
     fi
 }
 
 build_and_run() {
-    if [[ ${CONFIG[run]} == true ]]; then
+    if [[ ${ALK_OS_CLI_CONFIG[run]} == true ]]; then
         validate_configuration_exists
 
         pretty_info "Building AlkOS..."
-        base_runner "Failed to build AlkOS" true "${ALK_OS_CLI_BUILD_SCRIPT_PATH}" ${CONFIG[verbose]}
+        base_runner "Failed to build AlkOS" true "${ALK_OS_CLI_BUILD_SCRIPT_PATH}" ${ALK_OS_CLI_CONFIG[verbose]}
 
         pretty_info "Running AlkOS in QEMU"
         base_runner "Failed to run AlkOS in QEMU" true "${ALK_OS_CLI_QEMU_RUN_SCRIPT_PATH}" --verbose
@@ -175,7 +182,7 @@ build_and_run() {
 }
 
 setup_git_hooks() {
-  if [[ ${CONFIG[setup_hooks]} == true ]]; then
+  if [[ ${ALK_OS_CLI_CONFIG[setup_hooks]} == true ]]; then
     base_runner "Failed to setup git-hooks" true "${ALK_OS_CLI_SETUP_HOOKS_SCRIPT_PATH}"
   fi
 }
