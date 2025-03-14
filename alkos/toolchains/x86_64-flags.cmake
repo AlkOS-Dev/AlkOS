6b77c74c8a3fdf96c7195016659d02912544f1ba
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-03-14 19:31:56 +0100

    Fixed release

--- alkos/toolchains/x86_64-flags.cmake
+++ alkos/toolchains/x86_64-flags.cmake
@@ -1,3 +1,6 @@
+set(CMAKE_C_FLAGS_RELEASE "" CACHE STRING "Release C flags" FORCE)
+set(CMAKE_CXX_FLAGS_RELEASE "" CACHE STRING "Release C++ flags" FORCE)
+
 # Architecture specific flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wall -Wextra -ffreestanding -mstackrealign")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wall -Wextra -ffreestanding -fno-exceptions -fno-rtti -mstackrealign")
@@ -8,8 +11,24 @@ if (CMAKE_BUILD_TYPE STREQUAL "Debug" OR CMAKE_BUILD_TYPE STREQUAL "DEBUG")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0 -g -fno-inline -fstack-protector-all")
     set(CMAKE_ASM_NASM_FLAGS "${CMAKE_ASM_NASM_FLAGS} -g -F dwarf")
 elseif (CMAKE_BUILD_TYPE STREQUAL "Release" OR CMAKE_BUILD_TYPE STREQUAL "RELEASE")
-    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -O2")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O2")
+    set(SPEED_FLAGS "-O2")
+
+    # Remove unwanted optimisations
+    set(SPEED_FLAGS "${SPEED_FLAGS} -fno-strict-aliasing") # ERROR
+    set(SPEED_FLAGS "${SPEED_FLAGS} -fno-omit-frame-pointer") # UNSAFE
+    set(SPEED_FLAGS "${SPEED_FLAGS} -fno-tree-loop-vectorize") # ERROR
+    set(SPEED_FLAGS "${SPEED_FLAGS} -fno-tree-slp-vectorize") # ERROR
+    set(SPEED_FLAGS "${SPEED_FLAGS} -fno-delete-null-pointer-checks") # UNSAFE
+
+    # Add tempting ones
+    set(SPEED_FLAGS "${SPEED_FLAGS} -floop-unroll-and-jam")
+    set(SPEED_FLAGS "${SPEED_FLAGS} -fgcse-after-reload")
+    set(SPEED_FLAGS "${SPEED_FLAGS} -ftree-partial-pre")
+    set(SPEED_FLAGS "${SPEED_FLAGS} -fipa-cp-clone") # Allows function cloning - might be unhandy in debugging
+    set(SPEED_FLAGS "${SPEED_FLAGS} -fsplit-loops")
+
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${SPEED_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${SPEED_FLAGS}")
 else ()
     message(FATAL_ERROR "UNKNOWN BUILD TYPE: ${CMAKE_BUILD_TYPE}")
 endif ()
