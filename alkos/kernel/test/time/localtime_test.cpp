066e545d4a58ed9f6f97faeb4a2d5500fac10af2
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-03-14 14:52:09 +0100

    Clang-format fix

--- alkos/kernel/test/time/localtime_test.cpp
+++ alkos/kernel/test/time/localtime_test.cpp
@@ -19,7 +19,8 @@ class TimeTConversionTest : public TestGroupBase
         const int sec = 0
     )
     {
-        struct tm timeinfo{};
+        struct tm timeinfo;
+
         timeinfo.tm_year = year - 1900;
         timeinfo.tm_mon  = month - 1;
         timeinfo.tm_mday = day;
