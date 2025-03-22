d88c6888fd1228c3c859ed49edf7a41ebd2232a2
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-03-22 12:18:31 +0100

    Added -r --run flag

--- scripts/tests/regression/common/parsing.bash
+++ scripts/tests/regression/common/parsing.bash
@@ -4,7 +4,9 @@ PARSE_BASH_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && p
 
 source "${PARSE_BASH_DIR}/../../../utils/pretty_print.bash"
 
-VERBOSE_FLAG=""
+export VERBOSE_FLAG=""
+
+PARSING_RUN_PROVIDED=""
 
 help() {
     local script_name="$1"
@@ -13,6 +15,7 @@ help() {
 Usage: ${script_name} <architecture> [options]
 
 Options:
+    -r, --run                   Expected to run the script
     -v, --verbose               Enable verbose output
     -h, --help                  Display this help message
 
@@ -32,6 +35,10 @@ parse_args() {
                 help "${script_name}"
                 exit 0
                 ;;
+            -r|--run)
+                PARSING_RUN_PROVIDED=true
+                shift
+                ;;
             -v|--verbose)
                 VERBOSE_FLAG="-v --verbose"
                 shift
@@ -50,6 +57,12 @@ parse_args() {
 
     if [[ -z "$ARCH" ]]; then
         pretty_error "No architecture specified"
+        help "${script_name}"
+        exit 1
+    fi
+
+    if [[ -z "$PARSING_RUN_PROVIDED" ]]; then
+        pretty_error "Run flag must be provided to run the script!"
         help "${script_name}"
         exit 1
     fi
