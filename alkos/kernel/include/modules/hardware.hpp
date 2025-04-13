831ebc42d4b0e488c6db9cd4d3b7157c0d00caa2
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-04-13 23:50:37 +0200

    Added hardawre module

---
+++ alkos/kernel/include/modules/hardware.hpp
@@ -0,0 +1,32 @@
+#ifndef ALKOS_KERNEL_INCLUDE_MODULES_HARDWARE_HPP_
+#define ALKOS_KERNEL_INCLUDE_MODULES_HARDWARE_HPP_
+
+#include <extensions/template_lib.hpp>
+
+namespace internal
+{
+class HardwareModule : TemplateLib::StaticSingletonHelper
+{
+    // -------------------------------------
+    // Protected singleton constructor
+    // -------------------------------------
+
+    protected:
+    HardwareModule() noexcept;
+
+    // ------------------------------
+    // Getters
+    // ------------------------------
+
+    public:
+    // ------------------------------
+    // Module fields
+    // ------------------------------
+
+    private:
+};
+}  // namespace internal
+
+using HardwareModule = TemplateLib::StaticSingleton<internal::HardwareModule>;
+
+#endif  // ALKOS_KERNEL_INCLUDE_MODULES_HARDWARE_HPP_
