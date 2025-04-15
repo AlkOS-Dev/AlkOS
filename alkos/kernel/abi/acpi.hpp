341cc0f71a44fc51f061b5cf9781dc6c0c2e28e5
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-04-15 17:45:59 +0200

    Added acpi abi

--- alkos/kernel/abi/acpi.hpp
+++ alkos/kernel/abi/acpi.hpp
@@ -1,4 +1,20 @@
 #ifndef ALKOS_KERNEL_ABI_ACPI_HPP_
 #define ALKOS_KERNEL_ABI_ACPI_HPP_
 
+namespace arch
+{
+/* Should be defined by architecture, all Acpi logic correlated to arhcitecture and state should be
+ * stored here */
+class AcpiController;
+
+/* Abi which AcpiController class should follow */
+struct AcpiABI {
+    void ParseTables();
+};
+
+}  // namespace arch
+
+/* Load architecture definition of component */
+#include <abi/acpi.hpp>
+
 #endif  // ALKOS_KERNEL_ABI_ACPI_HPP_
