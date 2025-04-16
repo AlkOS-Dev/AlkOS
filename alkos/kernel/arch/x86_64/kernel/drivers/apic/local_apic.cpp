faa808e421d6599f4095af50b0a84c851672e4a7
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-04-16 14:31:36 +0200

    Added memory io header

--- alkos/kernel/arch/x86_64/kernel/drivers/apic/local_apic.cpp
+++ alkos/kernel/arch/x86_64/kernel/drivers/apic/local_apic.cpp
@@ -2,8 +2,20 @@
 #include "msrs.hpp"
 
 #include <assert.h>
+#include <drivers/pic8259/pic8259.hpp>
+#include <extensions/debug.hpp>
+#include <todo.hpp>
 
 void EnableLocalAPIC()
 {
     R_ASSERT_TRUE(IsApicSupported(), "APIC is not supported on this platform...");
+
+    /* Disable PIC unit */
+    Pic8259Disable();
+
+    TODO_WHEN_VMEM_WORKS
+    /* Map local apic address to vmem */
+    // TODO: currently: identity
+
+    TRACE_INFO("Local APIC found at address: %016X", GetLocalApicPhysicalAddress());
 }
--- alkos/kernel/arch/x86_64/kernel/drivers/apic/local_apic.hpp
+++ alkos/kernel/arch/x86_64/kernel/drivers/apic/local_apic.hpp
@@ -1,11 +1,18 @@
 #ifndef ALKOS_KERNEL_ARCH_X86_64_KERNEL_DRIVERS_APIC_LOCAL_APIC_HPP_
 #define ALKOS_KERNEL_ARCH_X86_64_KERNEL_DRIVERS_APIC_LOCAL_APIC_HPP_
 
+#include <assert.h>
 #include <cpuid.h>
+#include <extensions/bit.hpp>
 #include <extensions/defines.hpp>
 #include <extensions/types.hpp>
+#include "msrs.hpp"
 
 static constexpr u32 kEdxAcpiFlag = 1 << 9;
+
+static constexpr u32 kIA32ApicBaseMsr = 0x1B;
+
+static constexpr u64 kIA32ApicBaseMsrEnable = 0x800;
 
 NODISCARD FAST_CALL bool IsApicSupported()
 {
@@ -14,6 +21,18 @@ NODISCARD FAST_CALL bool IsApicSupported()
 
     __get_cpuid(1, &unused, &unused, &unused, &edx);
     return edx & kEdxAcpiFlag;
+}
+
+NODISCARD FAST_CALL u64 GetLocalApicPhysicalAddress()
+{
+    /* Return 4k page aligned address */
+    return CpuGetMSR(kIA32ApicBaseMsr) & ~kBitMaskRight<u64, 12>;
+}
+
+FAST_CALL void SetLocalApicPhysicalAddress(const u64 new_address)
+{
+    ASSERT_TRUE(IsAligned(new_address, 12), "Local APIC address is not aligned to 4k page!");
+    CpuSetMSR(kIA32ApicBaseMsr, new_address | kIA32ApicBaseMsrEnable);
 }
 
 void EnableLocalAPIC();
