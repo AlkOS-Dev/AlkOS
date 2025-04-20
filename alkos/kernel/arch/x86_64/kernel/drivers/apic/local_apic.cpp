1ab7860b12b855af7c5909d1468c1ef52bc65636
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-04-20 13:06:37 +0200

    fix spurios

--- alkos/kernel/arch/x86_64/kernel/drivers/apic/local_apic.cpp
+++ alkos/kernel/arch/x86_64/kernel/drivers/apic/local_apic.cpp
@@ -78,6 +78,7 @@ void LocalApic::Enable()
     /* Set the Spurious Interrupt Vector Register bit 8 to start receiving interrupts */
     auto reg    = ReadRegister<SpuriousInterruptRegister>(kSpuriousInterruptRegRW);
     reg.enabled = SpuriousInterruptRegister::State::kEnabled;
+    reg.vector  = kSpuriousVector;
 
     WriteRegister(kSpuriousInterruptRegRW, reg);
 
