6f314073123a3b1657d84bfbcb0a92097e9a86fe
F1r3d3v <ogieniewski.dev@gmail.com>
2025-07-25 01:38:06 +0200

    Add monostate

--- alkos/libc/include/extensions/utility.hpp
+++ alkos/libc/include/extensions/utility.hpp
@@ -1,6 +1,7 @@
 #ifndef ALKOS_LIBC_INCLUDE_EXTENSIONS_UTILITY_HPP_
 #define ALKOS_LIBC_INCLUDE_EXTENSIONS_UTILITY_HPP_
 
+#include <extensions/compare.hpp>
 #include <extensions/defines.hpp>
 #include <extensions/type_traits.hpp>
 
@@ -96,6 +97,18 @@ template <typename T>
 NODISCARD FORCE_INLINE_F constexpr T *addressof(T &arg) noexcept
 {
     return __builtin_addressof(arg);
+}
+
+// ------------------------------
+// std::monostate
+// ------------------------------
+
+struct monostate {
+};
+constexpr bool operator==(monostate, monostate) noexcept { return true; }
+constexpr strong_ordering operator<=>(monostate, monostate) noexcept
+{
+    return strong_ordering::equal;
 }
 
 }  // namespace std
