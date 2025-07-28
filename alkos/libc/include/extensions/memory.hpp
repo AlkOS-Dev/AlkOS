5daf7e41684ba46c9965a8a174b8f2fbfbe41c7b
F1r3d3v <ogieniewski.dev@gmail.com>
2025-07-28 03:19:23 +0200

    Add span

---
+++ alkos/libc/include/extensions/memory.hpp
@@ -0,0 +1,29 @@
+#ifndef ALKOS_LIBC_INCLUDE_EXTENSIONS_MEMORY_HPP_
+#define ALKOS_LIBC_INCLUDE_EXTENSIONS_MEMORY_HPP_
+
+#include <todo.h>
+
+namespace std
+{
+
+// ------------------------------
+// std::to_address
+// ------------------------------
+
+template <class T>
+constexpr T* to_address(T* p) noexcept
+{
+    static_assert(!std::is_function_v<T>);
+    return p;
+}
+
+template <class T>
+constexpr auto to_address(const T& p) noexcept
+{
+    TODO_LIBCPP_COMPLIANCE
+    return std::to_address(p.operator->());
+}
+
+}  // namespace std
+
+#endif  // ALKOS_LIBC_INCLUDE_EXTENSIONS_MEMORY_HPP_
