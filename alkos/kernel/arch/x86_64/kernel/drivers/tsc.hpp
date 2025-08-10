f70a60109ae84d19712796323beb337d7a5567ff
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-08-10 17:28:58 +0200

    Added comment

--- alkos/kernel/arch/x86_64/kernel/drivers/tsc.hpp
+++ alkos/kernel/arch/x86_64/kernel/drivers/tsc.hpp
@@ -3,6 +3,22 @@
 
 #include <cpu/control_registers.hpp>
 
+/**
+ * @file tsc.hpp
+ * @brief Time Stamp Counter (TSC) driver for x86_64.
+ *
+ * The Time Stamp Counter (TSC) is a 64-bit register present in all x86
+ * processors since the Pentium. It counts CPU cycles since the last reset,
+ * providing a high-resolution, low-overhead mechanism for timing. The RDTSC
+ * instruction reads the current value of the TSC.
+ *
+ * Modern Intel processors feature an invariant TSC, which increments at a
+ * constant rate regardless of changes in the CPU's core frequency. This makes
+ * it a reliable source for timekeeping.
+ *
+ * Reference: Intel 64 and IA-32 Architectures Software Developer's Manual,
+ * Volume 3A: System Programming Guide, Part 1.
+ */
 namespace tsc
 {
 
