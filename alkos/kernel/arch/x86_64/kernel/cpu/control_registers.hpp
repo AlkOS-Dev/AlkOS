3b27cb7b1a07aca92205f739d14e6ef962de9f45
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-08-10 17:32:44 +0200

    enum

--- alkos/kernel/arch/x86_64/kernel/cpu/control_registers.hpp
+++ alkos/kernel/arch/x86_64/kernel/cpu/control_registers.hpp
@@ -1,8 +1,11 @@
 #ifndef ALKOS_KERNEL_ARCH_X86_64_KERNEL_CPU_CONTROL_REGISTERS_HPP_
 #define ALKOS_KERNEL_ARCH_X86_64_KERNEL_CPU_CONTROL_REGISTERS_HPP_
 
+#include <extensions/types.hpp>
+
 namespace cpu
 {
+enum class ControlRegister : u8 { CR0, CR2, CR3, CR4 };
 
 }  // namespace cpu
 
