23f0efabcc362b3817092ac69b271529f12c1a56
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-08-13 18:34:51 +0200

    Added popcount

--- alkos/libc/include/extensions/bit.hpp
+++ alkos/libc/include/extensions/bit.hpp
@@ -15,9 +15,12 @@ namespace std
 
 namespace internal
 {
+// ------------------------------
+// countX implementations
+// ------------------------------
 
 template <std::unsigned_integral T>
-static constexpr int countl_one_constexpr(const T x)
+static constexpr int countl_one_constexpr(const T x) noexcept
 {
     if (x == kFullMask<T>) {
         return sizeof(T) * 8;
@@ -35,13 +38,13 @@ static constexpr int countl_one_constexpr(const T x)
 }
 
 template <std::unsigned_integral T>
-FAST_CALL constexpr int countl_zero_constexpr(const T x)
+FAST_CALL constexpr int countl_zero_constexpr(const T x) noexcept
 {
     return countl_one_constexpr(~x);
 }
 
 template <std::unsigned_integral T>
-static constexpr int countr_one_constexpr(const T x)
+static constexpr int countr_one_constexpr(const T x) noexcept
 {
     if (x == kFullMask<T>) {
         return sizeof(T) * 8;
@@ -59,15 +62,37 @@ static constexpr int countr_one_constexpr(const T x)
 }
 
 template <std::unsigned_integral T>
-FAST_CALL constexpr int countr_zero_constexpr(const T x)
+FAST_CALL constexpr int countr_zero_constexpr(const T x) noexcept
 {
     return countr_one_constexpr(~x);
+}
+
+// ------------------------------
+// popcount implementation
+// ------------------------------
+
+template <std::unsigned_integral T>
+constexpr int popcount_constexpr(const T x) noexcept
+{
+    if (x == 0) {
+        return 0;
+    }
+
+    T iter    = x;
+    int count = 0;
+
+    while (iter != 0) {
+        count += AreIntersecting(kLsb<T>, iter);
+        iter >>= 1;
+    }
+
+    return count;
 }
 
 }  // namespace internal
 
 // ------------------------------
-// std namespace
+// countX implementations
 // ------------------------------
 
 #define DEFINE_PUBLIC_FUNCTION(func_name)              \
@@ -86,6 +111,21 @@ DEFINE_PUBLIC_FUNCTION(countl_one)
 DEFINE_PUBLIC_FUNCTION(countl_zero)
 DEFINE_PUBLIC_FUNCTION(countr_one)
 DEFINE_PUBLIC_FUNCTION(countr_zero)
+
+// ------------------------------
+// popcount implementation
+// ------------------------------
+
+template <std::unsigned_integral T>
+constexpr FAST_CALL int popcount(T x) noexcept
+{
+    if (std::is_constant_evaluated()) {
+        return internal::popcount_constexpr(x);
+    } else {
+        TODO_OPTIMISE
+        return internal::popcount_constexpr(x);
+    }
+}
 
 }  // namespace std
 
