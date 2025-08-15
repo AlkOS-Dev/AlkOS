818716d9ca7405c74c4adf10145c30c25d7f50f6
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-08-15 02:53:51 +0200

    Added proper clock registry

--- alkos/kernel/include/hardware/clock_infra.hpp
+++ alkos/kernel/include/hardware/clock_infra.hpp
@@ -6,15 +6,22 @@
 namespace hardware
 {
 
-struct ClockRegistryEntry {
-    struct ClockCallbacks {
-        int (*enable)(ClockRegistryEntry* clock_entry);
-        int (*disable)(ClockRegistryEntry* clock_entry);
-    };
+struct ClockRegistryEntry : data_structures::RegistryEntry {
+    /* Clock numbers */
+    u64 frequency_kHz;               // Frequency in kHz
+    u64 uncertainty_margin_per_sec;  // Uncertainty margin in femto seconds per second
+
+    /* Callbacks */
+    u64 (*read)(ClockRegistryEntry* clock_entry);
+    u64 (*read_femto_seconds)(ClockRegistryEntry* clock_entry);
+    bool (*enable_device)(ClockRegistryEntry* clock_entry);
+    bool (*disable_device)(ClockRegistryEntry* clock_entry);
+    void (*stop_counter)(ClockRegistryEntry* clock_entry);
+    void (*resume_counter)(ClockRegistryEntry* clock_entry);
 };
 
 static constexpr size_t kMaxClocks = 8;
-using ClockRegistry                = data_structures::StaticVector<ClockRegistryEntry, kMaxClocks>;
+using ClockRegistry                = data_structures::Registry<ClockRegistryEntry, kMaxClocks>;
 
 }  // namespace hardware
 
