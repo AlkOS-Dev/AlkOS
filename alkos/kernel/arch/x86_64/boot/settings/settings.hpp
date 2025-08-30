c71bb3d1996de88f19d0b0459e46031da7c18d08
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2025-08-30 16:02:31 +0200

    style: Reorder include directives in settings.hpp

--- alkos/kernel/arch/x86_64/boot/settings/settings.hpp
+++ alkos/kernel/arch/x86_64/boot/settings/settings.hpp
@@ -1,8 +1,8 @@
 #ifndef ALKOS_BOOT_SETTINGS_HPP_
 #define ALKOS_BOOT_SETTINGS_HPP_
 
-#include <extensions/types.hpp>
 #include <extensions/bit.hpp>
+#include <extensions/types.hpp>
 
 static constexpr u64 kKernelVirtualAddressStart   = kBitMaskLeft<u64, 33>;
 static constexpr u64 kKernelDirectMapAddressStart = kBitMaskLeft<u64, 17>;
