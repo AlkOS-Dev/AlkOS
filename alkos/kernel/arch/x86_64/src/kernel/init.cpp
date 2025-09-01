e2337dda216e466765883b080023c6aea4ed9b3f
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2025-09-01 19:42:17 +0200

    refactor: Restructure PreKernelInit with improved error handling and logging

--- alkos/kernel/arch/x86_64/src/kernel/init.cpp
+++ alkos/kernel/arch/x86_64/src/kernel/init.cpp
@@ -6,16 +6,21 @@
 #error "AlkOS needs to be compiled with a x86_64-elf compiler"
 #endif
 
-/* internal includes */
 #include <cpuid.h>
 #include <extensions/debug.hpp>
 #include <extensions/expected.hpp>
 #include <extensions/internal/formats.hpp>
+
 #include <modules/hardware.hpp>
 #include <terminal.hpp>
-#include "cpu/utils.hpp"
 
-/* external init procedures */
+#include "cpu/utils.hpp"
+#include "panic.hpp"
+
+//==============================================================================
+// External Functions and Variables
+//==============================================================================
+
 extern "C" void EnableOSXSave();
 extern "C" void EnableSSE();
 extern "C" void EnableAVX();
@@ -28,43 +33,33 @@ static int GetCpuModel()
     return ebx;
 }
 
+//==================================================================================
+// Main Entry Point
+//==================================================================================
+
 extern "C" void PreKernelInit(void* loader_data)
 {
-    TODO_WHEN_DEBUGGING_FRAMEWORK
-
     arch::TerminalInit();
-    TRACE_INFO("In 64 bit mode");
+    TRACE_INFO("In PreKernelInit...");
 
     TRACE_INFO("CPU Model: %d / %08X", GetCpuModel(), GetCpuModel());
 
-    TRACE_INFO("Checking for LoaderData...");
     if (loader_data == nullptr) {
-        TRACE_ERROR("LoaderData check failed!");
-        OsHangNoInterrupts();
+        arch::KernelPanic("LoaderData is null");
     }
-    TRACE_SUCCESS("LoaderData found!");
-    TODO_WHEN_DEBUGGING_FRAMEWORK
-    TRACE_INFO("Starting pre-kernel initialization");
 
-    TRACE_INFO("Starting to setup CPU features");
+    TRACE_INFO("Setting up CPU features...");
     BlockHardwareInterrupts();
 
     /* NOTE: sequence is important */
-    TRACE_INFO("Setting up OS XSAVE...");
     EnableOSXSave();
-    TRACE_SUCCESS("OS XSAVE setup complete!");
-
-    TRACE_INFO("Setting up SSE...");
     EnableSSE();
-    TRACE_SUCCESS("SSE setup complete!");
-
-    TRACE_INFO("Setting up AVX...");
     EnableAVX();
-    TRACE_SUCCESS("AVX setup complete!");
 
     HardwareModule::Init();
     HardwareModule::Get().GetInterrupts().FirstStageInit();
 
     EnableHardwareInterrupts();
-    TRACE_INFO("Finished cpu features setup.");
+
+    TRACE_INFO("Leaving PreKernelInit and entering kernel...");
 }
