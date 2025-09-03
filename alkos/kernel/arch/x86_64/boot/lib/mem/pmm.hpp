eb99188cc7f601fbc7794146ac946558316126ca
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2025-09-03 23:17:23 +0200

    feat: Implement Physical Memory Manager initialization and integration

--- alkos/kernel/arch/x86_64/boot/lib/mem/pmm.hpp
+++ alkos/kernel/arch/x86_64/boot/lib/mem/pmm.hpp
@@ -4,6 +4,9 @@
 #include <extensions/bit.hpp>
 #include <extensions/bit_array.hpp>
 #include <extensions/expected.hpp>
+#include <extensions/internal/formats.hpp>
+
+#include <extensions/debug.hpp>
 
 #include "mem/error.hpp"
 #include "mem/physical_ptr.hpp"
@@ -13,55 +16,91 @@
 
 class PhysicalMemoryManager
 {
-    static constexpr u64 kPageSizeBytes = 1 < 12;
+    // TODO
+    // Should add overflow checked arithmetic to libc and use it here
+    //
+    static constexpr u64 kPageSizeBytes = 1 << 12;
 
+    enum {
+        BitMapFree     = 0,
+        BitMapReserved = 1,
+    };
+
+    public:
     std::expected<void, MemError> Init(Multiboot::MemoryMap memory_map, u64 lowest_safe_addr)
     {
         using namespace Multiboot;
+        TRACE_DEBUG("Initializing Physical Memory Manager...");
+
+        ASSERT_TRUE(!bitmap_view_initialized_, "PMM already initialized");
 
         // Calculate bitmap size
-        size_t avaliable_memory_bytes = 0;
-        for (MmapEntry entry : memory_map) {
-            if (entry.type == MmapEntry::kMemoryAvailable)
-                avaliable_memory_bytes += entry.len;
+        u64 maximum_physical_address = 0;
+        for (MmapEntry& entry : memory_map) {
+            maximum_physical_address = std::max(maximum_physical_address, entry.addr + entry.len);
         }
 
         // Round up division TODO Add to libc
-        const size_t total_pages = (avaliable_memory_bytes + kPageSizeBytes - 1) / kPageSizeBytes;
-        const size_t bitmap_size_bytes = (total_pages + 7) / 8;
+        const size_t total_pages =
+            static_cast<u64>(maximum_physical_address + kPageSizeBytes - 1) / kPageSizeBytes;
+        TRACE_DEBUG(
+            "Total memory size: %sB, total pages: %llu", FormatMetricUint(maximum_physical_address),
+            total_pages
+        );
+        const u64 bitmap_size_bytes = (total_pages + 7) / 8;
 
         // Find place for bitmap
         static constexpr u64 kInvalidBitmapAddr = static_cast<u64>(-1);
         u64 bitmap_addr                         = kInvalidBitmapAddr;
-        for (MmapEntry entry : memory_map) {
-            // TODO rework
-            if (entry.addr < lowest_safe_addr) {
+        for (MmapEntry& entry : memory_map) {
+            // Calculate the maximum possible start address for the bitmap within this entry
+            // ensuring it respects page alignment and fits within the entry.
+            u64 potential_bitmap_start =
+                AlignDown(entry.addr + entry.len - bitmap_size_bytes, kPageSizeBytes);
+
+            if (potential_bitmap_start < entry.addr ||
+                potential_bitmap_start + bitmap_size_bytes > entry.addr + entry.len) {
+                continue;  // Bitmap does not fit or crosses start boundary
+            }
+
+            if (potential_bitmap_start < lowest_safe_addr) {
                 continue;
             }
 
-            if (entry.len < bitmap_size_bytes) {
-                continue;
-            }
-
-            bitmap_addr = entry.len + entry.addr - bitmap_size_bytes;
-            bitmap_addr = AlignDown(bitmap_addr, kPageSizeBytes);
-
-            ASSERT_LE(bitmap_addr, entry.addr + entry.len - bitmap_size_bytes);
+            bitmap_addr = potential_bitmap_start;
             break;
         }
         if (bitmap_addr == kInvalidBitmapAddr) {
             return std::unexpected{MemError::OutOfMemory};
         }
 
-        bitmap_ = BitMapView{reinterpret_cast<void *>(bitmap_addr), total_pages};
+        // Initialize bitmap
+        bitmap_view_ = new (bitmap_view_storage_)
+            BitMapView{reinterpret_cast<void*>(bitmap_addr), total_pages};
+        bitmap_view_->SetAll(BitMapReserved);
+        bitmap_view_initialized_ = true;
+
+        // Free available memory
+        for (MmapEntry& entry : memory_map) {
+            if (entry.type == MmapEntry::kMemoryAvailable) {
+                u64 start_addr = entry.addr;
+                u64 end_addr   = entry.addr + entry.len;
+
+                Free(PhysicalPtr<void>{start_addr}, end_addr - start_addr);
+            }
+        }
 
         // Reserve itself
         Reserve(PhysicalPtr<void>{bitmap_addr}, bitmap_size_bytes);
+
+        ResetIterationIndex();
+
+        return {};
     }
 
     void Reserve(PhysicalPtr<void> addr, u64 size_bytes)
     {
-        for (u64 i = 0; i < size_bytes; i += kPageSizeBytes) {
+        for (u64 i = 0; i < AlignUp(size_bytes, kPageSizeBytes); i += kPageSizeBytes) {
             Reserve(PhysicalPtr<void>{addr.Value() + i});
         }
     }
@@ -69,13 +108,13 @@ class PhysicalMemoryManager
     void Reserve(PhysicalPtr<void> addr)
     {
         ASSERT_TRUE(IsAligned(addr.Value(), kPageSizeBytes));
-        ASSERT_TRUE(!bitmap_.Get(PageIndex(addr)), "Reserving already reserved page");
-        bitmap_.SetTrue(PageIndex(addr));
+        ASSERT_TRUE(!bitmap_view_->Get(PageIndex(addr)), "Reserving already reserved page");
+        bitmap_view_->SetTrue(PageIndex(addr));
     }
 
     void Free(PhysicalPtr<void> addr, u64 size_bytes)
     {
-        for (u64 i = 0; i < size_bytes; i += kPageSizeBytes) {
+        for (u64 i = 0; i < AlignUp(size_bytes, kPageSizeBytes); i += kPageSizeBytes) {
             Free(PhysicalPtr<void>{addr.Value() + i});
         }
     }
@@ -83,8 +122,8 @@ class PhysicalMemoryManager
     void Free(PhysicalPtr<void> addr)
     {
         ASSERT_TRUE(IsAligned(addr.Value(), kPageSizeBytes));
-        ASSERT_TRUE(bitmap_.Get(PageIndex(addr)), "Freeing already free page");
-        bitmap_.SetFalse(PageIndex(addr));
+        ASSERT_TRUE(bitmap_view_->Get(PageIndex(addr)), "Freeing already free page");
+        bitmap_view_->SetFalse(PageIndex(addr));
     }
 
     // TODO: Alloc contiguous memory
@@ -96,8 +135,8 @@ class PhysicalMemoryManager
             return std::unexpected{res.error()};
         }
 
-        ASSERT_TRUE(!bitmap_.Get(iteration_index_), "Allocating already allocated page");
-        bitmap_.SetTrue(iteration_index_);
+        ASSERT_TRUE(!bitmap_view_->Get(iteration_index_), "Allocating already allocated page");
+        bitmap_view_->SetTrue(iteration_index_);
         return PhysicalPtr<void>{iteration_index_ * kPageSizeBytes};
     }
 
@@ -111,12 +150,15 @@ class PhysicalMemoryManager
         return addr.Value() / kPageSizeBytes;
     }
 
+    FORCE_INLINE_F void ResetIterationIndex() { iteration_index_ = bitmap_view_->Size() - 1; }
+
     FORCE_INLINE_F std::expected<void, MemError> FindNextFreePage()
     {
-        const size_t total_pages = bitmap_.Size();
+        const size_t total_pages = bitmap_view_->Size();
         for (size_t i = 0; i < total_pages; i++) {
-            const size_t idx = (iteration_index_ + i) % total_pages;
-            if (!bitmap_.Get(idx)) {
+            // Reverse iteration to alloc higher addresses first
+            const size_t idx = (iteration_index_ - i) % total_pages;
+            if (!bitmap_view_->Get(idx)) {
                 iteration_index_ = idx;
                 return {};
             }
@@ -129,7 +171,10 @@ class PhysicalMemoryManager
     // Private fields
     //==============================================================================
 
-    BitMapView bitmap_;
+    BitMapView* bitmap_view_{nullptr};
+    alignas(64) byte bitmap_view_storage_[sizeof(BitMapView)]{};
+    bool bitmap_view_initialized_{false};
+
     size_t iteration_index_{0};
 };
 
--- alkos/kernel/arch/x86_64/boot/loader_32/src/main.cpp
+++ alkos/kernel/arch/x86_64/boot/loader_32/src/main.cpp
@@ -6,6 +6,7 @@
 #error "loader_32 needs to be compiled with a i386-elf compiler"
 #endif
 
+#include <extensions/algorithm.hpp>
 #include <extensions/debug.hpp>
 #include <extensions/defines.hpp>
 #include <extensions/internal/formats.hpp>
@@ -50,6 +51,7 @@ extern byte kLoaderPreAllocatedMemory[];
 //==============================================================================
 
 static TransitionData loader_data;
+alignas(64) byte kPmmPreAllocatedMemory[sizeof(PhysicalMemoryManager)];
 
 //==============================================================================
 // High-Level Boot Steps
@@ -78,6 +80,7 @@ static void InitializeAndVerifyEnvironment(u32 boot_loader_magic)
 static MemoryManager* SetupMemoryManagement(MultibootInfo& multiboot_info)
 {
     TRACE_DEBUG("Setting up memory management...");
+
     auto* memory_manager = new (kLoaderPreAllocatedMemory) MemoryManager();
 
     // Identity map the first 512 GiB of memory
@@ -98,6 +101,16 @@ static MemoryManager* SetupMemoryManagement(MultibootInfo& multiboot_info)
         if (entry.type == MmapEntry::kMemoryAvailable) {
             memory_manager->AddFreeMemoryRegion(entry.addr, entry.addr + entry.len);
         }
+    }
+    u64 lowest_safe_addr = std::max(
+        static_cast<u64>(reinterpret_cast<u32>(multiboot_header_end)),
+        static_cast<u64>(reinterpret_cast<u32>(loader_32_end))
+    );
+    auto* pmm = new (kPmmPreAllocatedMemory) PhysicalMemoryManager();
+
+    auto pmm_init_res = pmm->Init(MemoryMap(mmap_tag_res.value()), lowest_safe_addr);
+    if (!pmm_init_res) {
+        KernelPanic("Physical memory manager initialization failed!");
     }
 
     // Reserve memory currently in use by this loader
