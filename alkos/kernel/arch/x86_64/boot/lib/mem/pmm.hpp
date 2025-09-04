963b913bb0430318ea3d015cd005ea14e5d499bf
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2025-09-04 20:29:44 +0200

    refactor: Moved pmm implementation to a .cpp file

--- alkos/kernel/arch/x86_64/boot/lib/mem/pmm.hpp
+++ alkos/kernel/arch/x86_64/boot/lib/mem/pmm.hpp
@@ -29,85 +29,20 @@ class PhysicalMemoryManager
     };
 
     public:
-    std::expected<void, MemError> Init(Multiboot::MemoryMap mem_map, u64 lowest_safe_addr)
-    {
-        using namespace Multiboot;
-        TRACE_DEBUG("Initializing Physical Memory Manager...");
-        ASSERT_TRUE(!bitmap_view_initialized_, "PMM already initialized");
+    std::expected<void, MemError> Init(Multiboot::MemoryMap mem_map, u64 lowest_safe_addr);
 
-        // Calc size
-        TRACE_DEBUG("Calculating bitmap size...");
-        const auto [bitmap_size, total_pages] = CalcBitmapSize(mem_map);
+    void Reserve(PhysicalPtr<void> addr, u64 size);
 
-        // Find location
-        TRACE_DEBUG("Finding bitmap location...");
-        const auto bitmap_addr_res = FindBitmapLocation(mem_map, bitmap_size, lowest_safe_addr);
-        if (!bitmap_addr_res) {
-            return std::unexpected(bitmap_addr_res.error());
-        }
-        const u64 bitmap_addr = bitmap_addr_res.value();
+    void Reserve(PhysicalPtr<void> addr);
 
-        // Initialize bitmap
-        TRACE_DEBUG("Initializing bitmap view...");
-        InitBitmapView(bitmap_addr, bitmap_size);
+    void Free(PhysicalPtr<void> addr, u64 size);
 
-        // Free available memory
-        TRACE_DEBUG("Initializing free memory...");
-        InitFreeMemory(mem_map);
-
-        // Reserve itself
-        TRACE_DEBUG("Reserving bitmap memory...");
-        Reserve(PhysicalPtr<void>{bitmap_addr}, bitmap_size);
-
-        return {};
-    }
-
-    void Reserve(PhysicalPtr<void> addr, u64 size)
-    {
-        const u64 start_addr = AlignDown(addr.Value(), kPageSize);
-        const u64 end_addr   = AlignUp(addr.Value() + size, kPageSize);
-        for (u64 i = 0; i < end_addr - start_addr; i += kPageSize) {
-            Reserve(PhysicalPtr<void>{addr.Value() + i});
-        }
-    }
-
-    void Reserve(PhysicalPtr<void> addr)
-    {
-        ASSERT_TRUE(IsAligned(addr.Value(), kPageSize));
-        ASSERT_TRUE(!bitmap_view_->Get(PageIndex(addr)), "Reserving already reserved page");
-        bitmap_view_->SetTrue(PageIndex(addr));
-    }
-
-    void Free(PhysicalPtr<void> addr, u64 size)
-    {
-        const u64 start_addr = AlignDown(addr.Value(), kPageSize);
-        const u64 end_addr   = AlignUp(addr.Value() + size, kPageSize);
-        for (u64 i = 0; i < end_addr - start_addr; i += kPageSize) {
-            Free(PhysicalPtr<void>{addr.Value() + i});
-        }
-    }
-
-    void Free(PhysicalPtr<void> addr)
-    {
-        ASSERT_TRUE(IsAligned(addr.Value(), kPageSize));
-        ASSERT_TRUE(bitmap_view_->Get(PageIndex(addr)), "Freeing already free page");
-        bitmap_view_->SetFalse(PageIndex(addr));
-    }
+    void Free(PhysicalPtr<void> addr);
 
     // TODO: Alloc contiguous memory
     // TODO: Alloc page below 4GiB
 
-    std::expected<PhysicalPtr<void>, MemError> Alloc()
-    {
-        auto res = IterateToNextFreePage();
-        if (!res) {
-            return std::unexpected{res.error()};
-        }
-
-        ASSERT_TRUE(!bitmap_view_->Get(iteration_index_), "Allocating already allocated page");
-        bitmap_view_->SetTrue(iteration_index_);
-        return PhysicalPtr<void>{iteration_index_ * kPageSize};
-    }
+    std::expected<PhysicalPtr<void>, MemError> Alloc();
 
     private:
     //==============================================================================
@@ -138,95 +73,15 @@ class PhysicalMemoryManager
 
     // Init Helpers
 
-    std::tuple<u64, u64> CalcBitmapSize(Multiboot::MemoryMap& mem_map)
-    {
-        using namespace Multiboot;
-
-        u64 maximum_physical_address = 0;
-        for (MmapEntry& entry : mem_map) {
-            maximum_physical_address = std::max(maximum_physical_address, entry.addr + entry.len);
-        }
-
-        // Round up division TODO Add to libc
-        const size_t total_pages =
-            static_cast<u64>(maximum_physical_address + kPageSize - 1) / kPageSize;
-        TRACE_DEBUG(
-            "Total memory size: %sB, total pages: %llu", FormatMetricUint(maximum_physical_address),
-            total_pages
-        );
-        const u64 bitmap_size = (total_pages + 7) / 8;
-        return {bitmap_size, total_pages};
-    }
+    std::tuple<u64, u64> CalcBitmapSize(Multiboot::MemoryMap& mem_map);
 
     std::expected<u64, MemError> FindBitmapLocation(
         Multiboot::MemoryMap mem_map, u64 bitmap_size, u64 lowest_safe_addr
-    )
-    {
-        using namespace Multiboot;
+    );
 
-        std::optional<u64> bitmap_addr{};
-        for (MmapEntry& entry : mem_map) {
-            if (entry.type != MmapEntry::kMemoryAvailable) {
-                continue;
-            }
+    void InitBitmapView(const u64 addr, const u64 size);
 
-            if (entry.len < bitmap_size) {
-                continue;
-            }
-
-            // Calculate the maximum possible start address for the bitmap within this entry
-            // ensuring it respects page alignment and fits within the entry.
-            u64 potential_bitmap_start = AlignDown(entry.addr + entry.len - bitmap_size, kPageSize);
-
-            if (potential_bitmap_start < entry.addr ||
-                potential_bitmap_start + bitmap_size > entry.addr + entry.len) {
-                continue;  // Bitmap does not fit or crosses start boundary
-            }
-
-            if (potential_bitmap_start < lowest_safe_addr) {
-                continue;
-            }
-
-            // Ensure the entire bitmap is within the 32-bit address range
-            if (potential_bitmap_start + bitmap_size > kBitMask32) {
-                continue;
-            }
-
-            bitmap_addr.emplace(potential_bitmap_start);
-            break;
-        }
-        if (!bitmap_addr.has_value()) {
-            return std::unexpected{MemError::OutOfMemory};
-        }
-
-        return *bitmap_addr;
-    }
-
-    void InitBitmapView(const u64 addr, const u64 size)
-    {
-        TRACE_DEBUG("Bitmap located at: %#018llx, size: %llu bytes", addr, size);
-        bitmap_view_ = new (bitmap_view_storage_) BitMapView{reinterpret_cast<void*>(addr), size};
-        bitmap_view_->SetAll(BitMapReserved);
-        bitmap_view_initialized_ = true;
-
-        for (size_t i = 0; i < bitmap_view_->Size(); ++i) {
-            ASSERT_TRUE(bitmap_view_->Get(i), "Bitmap not initialized correctly");
-        }
-    }
-
-    void InitFreeMemory(Multiboot::MemoryMap& mem_map)
-    {
-        using namespace Multiboot;
-
-        for (MmapEntry& entry : mem_map) {
-            if (entry.type == MmapEntry::kMemoryAvailable) {
-                u64 start_addr = AlignUp<u64>(entry.addr, kPageSize);
-                u64 end_addr   = AlignDown<u64>(entry.addr + entry.len, kPageSize);
-
-                Free(PhysicalPtr<void>{start_addr}, end_addr - start_addr);
-            }
-        }
-    }
+    void InitFreeMemory(Multiboot::MemoryMap& mem_map);
 
     //==============================================================================
     // Private fields
