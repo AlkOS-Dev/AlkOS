fb1b0d0ec434f17ff4ce64d736b6f5d502bf0b95
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-10-19 01:37:43 +0200

    Added hal files

--- alkos/kernel/arch/x86_64/src/drivers/apic/local_apic.hpp
+++ alkos/kernel/arch/x86_64/src/drivers/apic/local_apic.hpp
@@ -1,5 +1,5 @@
-#ifndef ALKOS_KERNEL_ARCH_X86_64_KERNEL_DRIVERS_APIC_LOCAL_APIC_HPP_
-#define ALKOS_KERNEL_ARCH_X86_64_KERNEL_DRIVERS_APIC_LOCAL_APIC_HPP_
+#ifndef ALKOS_KERNEL_ARCH_X86_64_SRC_DRIVERS_APIC_LOCAL_APIC_HPP_
+#define ALKOS_KERNEL_ARCH_X86_64_SRC_DRIVERS_APIC_LOCAL_APIC_HPP_
 
 #include <assert.h>
 #include <cpuid.h>
@@ -9,6 +9,7 @@
 #include <todo.hpp>
 #include "cpu/msrs.hpp"
 #include "include/memory_io.hpp"
+#include "interrupts/interrupt_types.hpp"
 
 /**
  * TODO: SUPPORT 2x APIC
@@ -418,5 +419,7 @@ class LocalApic
     protected:
     bool is_enabled_{};
     u64 local_apic_physical_address_{};
+    // intr::InterruptDriver::callbacks cbs_{};
+    // intr::InterruptDriver driver_{};
 };
-#endif  // ALKOS_KERNEL_ARCH_X86_64_KERNEL_DRIVERS_APIC_LOCAL_APIC_HPP_
+#endif  // ALKOS_KERNEL_ARCH_X86_64_SRC_DRIVERS_APIC_LOCAL_APIC_HPP_
