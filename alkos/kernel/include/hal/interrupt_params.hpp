328a51dfa871de10edfe4e230a4e8606d5aad54f
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-10-19 12:43:32 +0200

    Provided mapping info to kernel

--- alkos/kernel/include/hal/interrupt_params.hpp
+++ alkos/kernel/include/hal/interrupt_params.hpp
@@ -5,10 +5,15 @@
 
 namespace hal
 {
+/* Interrupt layout specific params */
 using arch::ExceptionData;
 static constexpr size_t kNumExceptionHandlers  = arch::kNumExceptionHandlers;
 static constexpr size_t kNumHardwareInterrupts = arch::kNumHardwareInterrupts;
 static constexpr size_t kNumSoftwareInterrupts = arch::kNumSoftwareInterrupts;
+
+/* Mapping params */
+static constexpr u16 kTimerHwLirq      = 0;
+static constexpr u16 kPageFaultExcLirq = 14;
 }  // namespace hal
 
 #endif  // ALKOS_KERNEL_INCLUDE_HAL_INTERRUPT_PARAMS_HPP_
