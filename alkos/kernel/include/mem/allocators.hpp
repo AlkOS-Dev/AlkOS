19cea67d135dbf8d02a64c0dc4d8c84966e154b9
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-10-20 21:24:35 +0200

    Alloca

--- alkos/kernel/include/mem/allocators.hpp
+++ alkos/kernel/include/mem/allocators.hpp
@@ -1,4 +1,26 @@
 #ifndef ALKOS_KERNEL_INCLUDE_MEM_ALLOCATORS_HPP_
 #define ALKOS_KERNEL_INCLUDE_MEM_ALLOCATORS_HPP_
 
+namespace alloca
+{
+template <class T>
+class DynArray
+{
+    public:
+    // ------------------------------
+    // Class creation
+    // ------------------------------
+
+    // ------------------------------
+    // Class interaction
+    // ------------------------------
+
+    // ------------------------------
+    // Class fields
+    // ------------------------------
+
+    private:
+};
+}  // namespace alloca
+
 #endif  // ALKOS_KERNEL_INCLUDE_MEM_ALLOCATORS_HPP_
