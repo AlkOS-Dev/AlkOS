688cf3773b4224e45d1f9470cfa1efe45fbf03de
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-10-22 23:32:09 +0200

    Added hal files

--- alkos/kernel/include/hal/debug.hpp
+++ alkos/kernel/include/hal/debug.hpp
@@ -1,4 +1,12 @@
 #ifndef ALKOS_KERNEL_INCLUDE_HAL_DEBUG_HPP_
 #define ALKOS_KERNEL_INCLUDE_HAL_DEBUG_HPP_
 
+#include <hal/impl/debug.hpp>
+
+namespace hal
+{
+using arch::Core;
+using arch::CoreConfig;
+}  // namespace hal
+
 #endif  // ALKOS_KERNEL_INCLUDE_HAL_DEBUG_HPP_
