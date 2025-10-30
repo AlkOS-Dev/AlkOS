fd4b6e264c4214b7468f9a8b25018c13b47358ec
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-10-30 21:55:00 +0100

    Added memory fences

--- alkos/kernel/arch/x86_64/src/hal/impl/sync.hpp
+++ alkos/kernel/arch/x86_64/src/hal/impl/sync.hpp
@@ -1,4 +1,15 @@
 #ifndef ALKOS_KERNEL_ARCH_X86_64_SRC_HAL_IMPL_SYNC_HPP_
 #define ALKOS_KERNEL_ARCH_X86_64_SRC_HAL_IMPL_SYNC_HPP_
 
+#include <extensions/defines.hpp>
+
+namespace arch
+{
+FAST_CALL void FullMemFence() { __asm__ volatile("mfence" ::: "memory"); }
+
+FAST_CALL void LoadMemFence() { __asm__ volatile("lfence" ::: "memory"); }
+
+FAST_CALL void SaveMemFence() { __asm__ volatile("" ::: "memory"); }
+}  // namespace arch
+
 #endif  // ALKOS_KERNEL_ARCH_X86_64_SRC_HAL_IMPL_SYNC_HPP_
--- alkos/kernel/include/hal/sync.hpp
+++ alkos/kernel/include/hal/sync.hpp
@@ -3,4 +3,13 @@
 
 #include <hal/impl/sync.hpp>
 
+namespace hal
+{
+WRAP_CALL void FullMemFence() { arch::FullMemFence(); }
+
+WRAP_CALL void LoadMemFence() { arch::LoadMemFence(); }
+
+WRAP_CALL void SaveMemFence() { arch::SaveMemFence(); }
+}  // namespace hal
+
 #endif  // ALKOS_KERNEL_INCLUDE_HAL_SYNC_HPP_
