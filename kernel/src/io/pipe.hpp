8c2450f1a043cbcb40296a9d10a413b9dc274d5f
Łukasz Kryczka <lukasz.kryczka000@gmail.com>
2025-12-01 23:10:42 +0100

    feat: Implement lock-free SPSC atomic cyclic buffer and refactor pipe to use it

--- kernel/src/io/pipe.hpp
+++ kernel/src/io/pipe.hpp
@@ -1,15 +1,15 @@
 #ifndef KERNEL_SRC_IO_PIPE_HPP_
 #define KERNEL_SRC_IO_PIPE_HPP_
 
-#include <algorithm.hpp>
+#include <data_structures/atomic_cyclic_buffer.hpp>
 #include <hal/spinlock.hpp>
-#include <memory.hpp>
 
 #include "io/stream.hpp"
 
 namespace IO
 {
 
+/// Assumes Single Producer - Single Consumer
 template <size_t Size>
 class Pipe : public IStream
 {
@@ -19,19 +19,7 @@ class Pipe : public IStream
 
     IoResult Write(std::span<const byte> buffer) override
     {
-        std::lock_guard guard(lock_);
-
-        size_t bytes_written = 0;
-        for (const auto &b : buffer) {
-            if (IsFull()) {
-                // Caller decides if they want to retry (spin) or yield.
-                break;
-            }
-            buffer_[head_] = b;
-            head_          = (head_ + 1) % Size;
-            count_++;
-            bytes_written++;
-        }
+        size_t bytes_written = buffer_.Write(buffer);
 
         if (bytes_written == 0 && buffer.size() > 0) {
             return std::unexpected(Error::Retry);
@@ -41,18 +29,7 @@ class Pipe : public IStream
 
     IoResult Read(std::span<byte> buffer) override
     {
-        std::lock_guard guard(lock_);
-
-        size_t bytes_read = 0;
-        for (size_t i = 0; i < buffer.size(); ++i) {
-            if (IsEmpty()) {
-                break;
-            }
-            buffer[i] = buffer_[tail_];
-            tail_     = (tail_ + 1) % Size;
-            count_--;
-            bytes_read++;
-        }
+        size_t bytes_read = buffer_.Read(buffer);
 
         if (bytes_read == 0 && buffer.size() > 0) {
             return std::unexpected(Error::Retry);
@@ -61,16 +38,7 @@ class Pipe : public IStream
     }
 
     private:
-    bool IsFull() const { return count_ == Size; }
-    bool IsEmpty() const { return count_ == 0; }
-
-    // Ring Buffer State
-    byte buffer_[Size];
-    size_t head_  = 0;  // Write index
-    size_t tail_  = 0;  // Read index
-    size_t count_ = 0;
-
-    hal::Spinlock lock_;
+    data_structures::AtomicCyclicBuffer<byte, Size> buffer_;
 };
 
 }  // namespace IO
