4535f2b337d76d5309a41b6056cd1006fe7daf9f
F1r3d3v <ogieniewski.dev@gmail.com>
2025-12-11 02:16:09 +0100

    refactor: improve internal utility types and span storage
    Refactor `OptionalField` in `utils.hpp` from a type alias to a macro to ensure `NO_UNIQUE_ADDRESS` is correctly applied with `std::conditional_t`.

--- libs/libtemplate/include/template/utils.hpp
+++ libs/libtemplate/include/template/utils.hpp
@@ -1,15 +1,17 @@
 #ifndef LIBS_LIBTEMPLATE_INCLUDE_TEMPLATE_UTILS_HPP_
 #define LIBS_LIBTEMPLATE_INCLUDE_TEMPLATE_UTILS_HPP_
 
-#include <variant.hpp>
+#include <defines.hpp>
+#include <type_traits.hpp>
 
 namespace template_lib
 {
 // ------------------------------
-// OptionalField
+// OPTIONAL_FIELD
 // ------------------------------
 
-template <bool cond, class T>
-using OptionalField = std::conditional_t<cond, T, std::monostate>;
+// Apparently using OptionalField = std::conditional_t<...> does not work
+#define OPTIONAL_FIELD(cond, type) NO_UNIQUE_ADDRESS std::conditional_t<cond, type, UNIQUE_EMPTY>
+
 }  // namespace template_lib
 #endif  // LIBS_LIBTEMPLATE_INCLUDE_TEMPLATE_UTILS_HPP_
