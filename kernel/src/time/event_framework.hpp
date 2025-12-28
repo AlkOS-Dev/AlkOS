9b51cf270c28f24efec82a7ec18b0921c163b541
Jakub Lisowski <Jlisowskyy@gmail.com>
2025-12-28 15:26:22 +0100

    Added event framework file

---
+++ kernel/src/time/event_framework.hpp
@@ -0,0 +1,13 @@
+#ifndef KERNEL_SRC_TIME_EVENT_FRAMEWORK_HPP_
+#define KERNEL_SRC_TIME_EVENT_FRAMEWORK_HPP_
+
+namespace timing
+{
+
+class EventFramework
+{
+};
+
+}  // namespace timing
+
+#endif  // KERNEL_SRC_TIME_EVENT_FRAMEWORK_HPP_
