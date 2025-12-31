a7536bf0b0c80d54f13770370f3d8a3e8ad0854b
F1r3d3v <ogieniewski.dev@gmail.com>
2025-12-31 13:15:46 +0100

    userspace

--- userspace/programs/hello_world/main.cpp
+++ userspace/programs/hello_world/main.cpp
@@ -7,29 +7,37 @@ void printf(const char *format, Args... args)
 {
     char buffer[256];
     snprintf(buffer, sizeof(buffer), format, args...);
-    __platform_write_console(buffer);
+    __platform_debug_write(buffer);
 }
 
 extern "C" int main()
 {
-    __platform_write_console(
+    printf(
         "\n----------------------------------\n"
         "Hello from User Space via Syscall!\n"
         "----------------------------------\n"
     );
 
-    printf("\nFormatting Test: %d + %d = %d\n\n", 2, 2, 4);
+    printf("Formatting Test: %d + %d = %d\n", 2, 2, 4);
 
     Timezone tz;
     __platform_get_timezone(&tz);
-    printf("Timezone offset (minutes): %d\n\n", tz.west_offset_minutes);
+    printf("Timezone offset (minutes): %d\n", tz.west_offset_minutes);
 
     u64 ticks = __platform_get_clock_ticks_in_second(kTimeUtc);
-    printf("Ticks per second (UTC): %llu\n\n", ticks);
+    printf("Ticks per second (UTC): %llu\n", ticks);
 
     TimeVal tv;
     __platform_get_clock_value(kTimeUtc, &tv, &tz);
-    printf("Current Timestamp: %llu\n\n", tv.seconds);
+    printf("Current Timestamp: %llu\n", tv.seconds);
+
+    while (true) {
+        static size_t kSpins = 1'000'000;
+        size_t counter       = 0;
+        for (size_t i = 0; i < kSpins; i++) {
+            ++counter;
+        }
+    }
 
     return 0;
 }
