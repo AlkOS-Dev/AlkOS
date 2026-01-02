ea7d7e0faf4184bf0a46b2476dc69f4bee1313db
Jakub Lisowski <Jlisowskyy@gmail.com>
2026-01-02 18:51:09 +0100

    Alkos init

--- libs/libc/src/include/sys/alkos.h
+++ libs/libc/src/include/sys/alkos.h
@@ -1,4 +1,15 @@
 #ifndef LIBS_LIBC_SRC_INCLUDE_SYS_ALKOS_H_
 #define LIBS_LIBC_SRC_INCLUDE_SYS_ALKOS_H_
 
+/**
+ * @brief This file collects all the system calls that are used by the libc.
+ *
+ * Calls should be specified in other header files and then included here.
+ */
+
+#include <sys/calls/fd.h>
+#include <sys/calls/proc.h>
+#include <sys/calls/thread.h>
+#include <sys/calls/time.h>
+
 #endif  // LIBS_LIBC_SRC_INCLUDE_SYS_ALKOS_H_
--- libs/libc/src/include/sys/calls.h
+++ libs/libc/src/include/sys/calls.h
@@ -58,15 +58,4 @@ enum SyscallNumber {
         (void)_SYSCALL_DISPATCH(num, FOR_EACH_PAIR(GET_SECOND_ARG, __VA_ARGS__)); \
     }
 
-/**
- * @brief This file collects all the system calls that are used by the libc.
- *
- * Calls should be specified in other header files and then included here.
- */
-
-#include <sys/calls/fd.h>
-#include <sys/calls/proc.h>
-#include <sys/calls/thread.h>
-#include <sys/calls/time.h>
-
 #endif  // LIBS_LIBC_SRC_INCLUDE_SYS_CALLS_H_
