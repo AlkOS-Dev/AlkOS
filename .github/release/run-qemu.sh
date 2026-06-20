1d28b8d8dbf39c01e1623cc33ff511a510dbe28c
Jakub Lisowski <Jlisowskyy@gmail.com>
2026-06-20 15:09:13 +0200

    simplify qemu script

--- .github/release/run-qemu.sh
+++ .github/release/run-qemu.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # Boot a released AlkOS ISO in QEMU.
 # Usage: ./run-qemu.sh [alkos.iso]   (defaults to the first alkos-*.iso here)
-# On Linux, add for speed:  -enable-kvm -cpu max,+invtsc -machine accel=kvm
 set -e
 
 ISO="${1:-$(ls alkos-*.iso 2>/dev/null | head -n1)}"
@@ -10,4 +9,5 @@ qemu-system-x86_64 \
   -cdrom "$ISO" \
   -m 4G -smp 4 \
   -cpu max -machine q35 \
-  -serial stdio -no-reboot
+  -serial stdio -no-reboot \
+  # -enable-kvm  # Linux: uncomment for KVM acceleration (much faster)
